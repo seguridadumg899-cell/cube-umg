930a2c3d8137f521f92d302d51fdf01bacad910a
seguridadumg899-cell <seguridadumg899@gmail.com>
2025-10-24 22:08:40 -0600

    Update head.php

--- head.php
+++ head.php
@@ -8,7 +8,6 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-<script src="https://cdn.tailwindcss.com"></script>
 <script>tailwind.config={theme:{extend:{fontFamily:{sans:['Inter','ui-sans-serif','system-ui']}}}}</script>
 </head>
 <body class="bg-slate-50 text-slate-900 font-sans">
